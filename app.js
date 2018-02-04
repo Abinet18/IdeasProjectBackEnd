50a18d1794e7b9f574b18cbd3a3f15ccc66a4c53
unknown <abinet.tafa@gmail.com>
2018-02-04 15:20:14 -0600

    register user

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app=express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
+app.use(cors({origin:'http://localhost:4200'}));
 app.use(expressValidator());
 
 app.use('/users',userRoutes);
