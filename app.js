b6a66d7dacfe3cec103f0e27eccab93360a51bdf
unknown <abinet.tafa@gmail.com>
2018-02-04 12:17:53 -0600

    mongoose integrated

---
+++ app.js
@@ -0,0 +1,35 @@
+var express=require('express');
+var logger=require('morgan');
+var cors=require('cors');
+var expressValidator=require('express-validator');
+var mongoose=require('mongoose');
+const User=require('./models/user');
+mongoose.connect('mongodb://localhost/ideadb',(err)=>{
+    if(err) console.log("Couldnot connect to database");
+    else console.log("connected");
+});
+
+var app=express();
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(expressValidator());
+
+app.get('/',(req,res)=>{
+    
+    const test=new User({email:"abinet.tafa@gmail.com",username:"abint",password:"pass"});
+    test.save((err)=>{console.log(err);});
+    res.send("Hello");
+})
+app.get('/user',(req,res)=>{
+    
+    User.findOne({}).then((data)=>{res.json(data)});
+    
+})
+
+
+app.listen(8000);
---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+mongoose.Promise = global.Promise; // Configure Mongoose Promises
+const Schema = mongoose.Schema; // Import Schema from Mongoose
+const crypto = require('crypto'); // A native JS bcrypt library for NodeJS
+
+const userSchema = new Schema({
+    email: { type: String, required: true, unique: true, lowercase: true },
+    username: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true }
+  });
+
+
+module.exports = mongoose.model('User', userSchema);
