fd9357e47c2a3993a7e7fb5edc863bb607453cde
unknown <abinet.tafa@gmail.com>
2018-02-04 19:00:52 -0600

    login added

--- models/user.js
+++ models/user.js
@@ -8,6 +8,8 @@ const userSchema = new Schema({
     username: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true }
   });
-
+  userSchema.methods.comparePassword = function(password) {
+    return password==this.password; // Return comparison of login password to password in database (true or false)
+  };
 
 module.exports = mongoose.model('User', userSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ router.get('/',(req,res)=>{
     User.findOne({}).then((data)=>{res.json(data)});
     
 })
-router.post('/',(req,res)=>
+router.post('/add',(req,res)=>
 {
     //validate before insert
     const user=new User({email:req.body.email,username:req.body.username,password:req.body.password});
@@ -21,5 +21,82 @@ router.post('/',(req,res)=>
             res.json({success:true});
         }})
 })
+
+/* ========
+  LOGIN ROUTE
+  ======== */
+  router.post('/login', (req, res) => {
+   
+        User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+          // Check if error was found
+          if (err) {
+            res.json({ success: false, message: err }); // Return error
+          } else {
+            // Check if username was found
+            if (!user) {
+              res.json({ success: false, message: 'Username not found.' }); // Return error
+            } else {
+              const validPassword = user.comparePassword(req.body.password); // Compare password provided to password in database
+              // Check if password is a match
+              if (!validPassword) {
+                res.json({ success: false, message: 'Password invalid' }); // Return error
+              } else {
+                const token = jwt.sign({ userId: user._id }, "dnm", { expiresIn: '24h' }); // Create a token for client
+                res.json({
+                  success: true,
+                  message: 'Success!',
+                  token: token,
+                  user: {
+                    username: user.username
+                  }
+                }); // Return success and token to frontend
+              }
+            }
+          }
+        });
+        
+  });
+
+  /* ================================================
+  MIDDLEWARE - Used to grab user's token from headers
+  ================================================ */
+  router.use((req, res, next) => {
+    const token = req.headers['authorization']; // Create token found in headers
+    // Check if token was found in headers
+    if (!token) {
+      res.json({ success: false, message: 'No token provided' }); // Return error
+    } else {
+      // Verify the token is valid
+      jwt.verify(token, config.secret, (err, decoded) => {
+        // Check if error is expired or invalid
+        if (err) {
+          res.json({ success: false, message: 'Token invalid: ' + err }); // Return error for token validation
+        } else {
+          req.decoded = decoded; // Create global variable to use in any request beyond
+          next(); // Exit middleware
+        }
+      });
+    }
+  });
+
+  /* ===============================================================
+     Route to get user's profile data
+  =============================================================== */
+  router.get('/profile', (req, res) => {
+    // Search for user in database
+    User.findOne({ _id: req.decoded.userId }).select('username email').exec((err, user) => {
+      // Check if error connecting
+      if (err) {
+        res.json({ success: false, message: err }); // Return error
+      } else {
+        // Check if user was found in database
+        if (!user) {
+          res.json({ success: false, message: 'User not found' }); // Return error, user was not found in db
+        } else {
+          res.json({ success: true, user: user }); // Return success, send user object to frontend for profile
+        }
+      }
+    });
+  });
 
 module.exports=router;
