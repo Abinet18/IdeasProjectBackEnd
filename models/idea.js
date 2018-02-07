33851050acd89607aaebd88a04c1f1dbb4ac6bb2
Abinet18 <35442169+Abinet18@users.noreply.github.com>
2018-02-07 08:22:15 -0600

    Update idea.js

--- models/idea.js
+++ models/idea.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose'); // Node Tool for MongoDB
 mongoose.Promise = global.Promise; // Configure Mongoose Promises
 const Schema = mongoose.Schema; // Import Schema from Mongoose
-const crypto = require('crypto'); // A native JS bcrypt library for NodeJS
-
+const crypto = require('crypto'); // A native script for encryption
+//Thought or comment schema
 const Thought = new Schema({
   owner:String,
   text:String,
   dateofth:Date
 })
+
+//Rating schema
 const Rating = new Schema({
   rater:String,
   rating:{ type:Number,min:0,max:5},
   dateofr:{type:Date}
 })
+
+//Idea schema
 const ideaSchema = new Schema({
     title: { type: String, required: true, unique: true, lowercase: false },
     type: { type: String, required: true, lowercase: false },
@@ -20,8 +24,8 @@ const ideaSchema = new Schema({
     owner: {type:String,required:true},
     dateApproved:{ type:Date},
     dateCreated:{ type:Date,required:true },    
-    thoughts:{type:[Thought]},
-    ratings:{type:[Rating]},
+    thoughts:{type:[Thought]},//refers to the thought schema above
+    ratings:{type:[Rating]},//refers to the rating schema above
     total:{type:Number},
     rateCount:{type:Number},
     commentCount:{type:Number}
